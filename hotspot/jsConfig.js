94c2430f0084b9c17ffdadb82e2c90495aba1476
Edmar Lozada <134915483+edmarlozada@users.noreply.github.com>
2026-01-12 08:44:23 +0800

    Add files via upload

---
+++ hotspot/jsConfig.js
@@ -0,0 +1,10 @@
+/* jsConfig.js  */
+/* Edmar Lozada */
+
+/* ===================================== */
+/* main vendo config                     */
+/* ===================================== */
+
+/* ip address of single vendo machine */
+   cfg.VendoIP = "10.0.0.2"; // ( required )
+
